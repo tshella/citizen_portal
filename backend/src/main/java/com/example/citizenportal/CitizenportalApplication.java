7a525b21af1e8d743506df5a72fc1fa6cb23e966
tshella <therealanthony43@gmail.com>
2025-05-25 13:14:36 -0400

    restructured

---
+++ backend/src/main/java/com/example/citizenportal/CitizenportalApplication.java
@@ -0,0 +1,13 @@
+package com.example.citizenportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenportalApplication.class, args);
+	}
+
+}
